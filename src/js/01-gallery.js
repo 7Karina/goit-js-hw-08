c50fb72e60a38a3fa9e2b1bfc9e42891a19e3142
@7Karina <karinafrank750@gmail.com>
2023-10-14 18:34:46 +0300

    14.10

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+  enableKeyboard: true,
+});
