219b080ec023d9a1c2a754915123161c50b7c8f5
@7Karina <karinafrank750@gmail.com>
2023-10-17 14:55:21 +0300

    14:55

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,28 +1,5 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
-const galleryRef = document.querySelector('.gallery');
-
-const markup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-      <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>
-    `;
-  })
-  .join('');
-
-galleryRef.insertAdjacentHTML('beforeend', markup);
-
-const gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  close: false,
-  enableKeyboard: true,
-});
-console.log(gallery);
+console.log(galleryItems);
