3099ac5f3a506e2f2506a979ad8dfcb5574d7cf6
@7Karina <karinafrank750@gmail.com>
2023-10-18 19:30:33 +0300

    config

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,28 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    player.getCurrentTime().then(function (seconds) {
-      console.log(seconds);
-      localStorage.setItem('videoplayer-current-time', seconds);
-    });
-  }, 1000)
-);
+const onPlay = function (data) {
+  player.getCurrentTime().then(function (seconds) {
+    console.log(seconds);
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000);
+};
+
+player.on('videoplayer-current-time', onPlay);
 
 player
   .setCurrentTime(30.456)
   .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
+    seconds = 0;
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
