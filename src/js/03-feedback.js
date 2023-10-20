7ab543fea9873bb14312f4873f5cd5dc91f18b2b
@7Karina <karinafrank750@gmail.com>
2023-10-20 22:26:14 +0300

    add_task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -44,3 +44,27 @@ function handlerInput(event) {
   userData[formElName] = formElValue;
   save(LOCAL_STORAGE_KEY, userData);
 }
+
+function save(key, value) {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+function remove(key) {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.log('Remove item error: ', error.message);
+  }
+}
