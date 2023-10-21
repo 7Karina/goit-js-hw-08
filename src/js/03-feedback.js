c621c95c07147af3e062684dc386f76bbd0a54c3
@7Karina <karinafrank750@gmail.com>
2023-10-21 17:13:32 +0300

    hw8_task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,23 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(handlerInput, 500));
 form.addEventListener('submit', handlerSubmit);
 document.addEventListener('DOMContentLoaded', loadForm);
-
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-
+let userData = {};
 function handlerSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, message },
   } = event.currentTarget;
   if (email.value === '' || message.value === '') {
-    return alert('fill all fiels');
+    return alert('fill all fields');
   }
   console.log(userData);
-  removeEventListener(LOCAL_STORAGE_KEY);
+  remove(LOCAL_STORAGE_KEY);
   event.currentTarget.reset();
   userData = {};
 }
-
 function loadForm() {
   const userDataFromLS = load(LOCAL_STORAGE_KEY);
   if (!userDataFromLS) {
@@ -36,7 +33,6 @@ function loadForm() {
     }
   }
 }
-
 function handlerInput(event) {
   const target = event.target;
   const formElValue = target.value;
